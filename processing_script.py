a48842fca268c33f1c1e4b278a923338fee4b9f8
rachanack-rachhu <rachanack123@gmail.com>
2025-11-20 23:55:33 +0530

    Delete processing_script.py

--- processing_script.py
+++
@@ -1,7 +0,0 @@
-import argparse
-print("--- Cloud Build Python script has started ---")
-parser = argparse.ArgumentParser()
-parser.add_argument('--file-path', required=True, help='The GCS path of the input file.')
-args = parser.parse_args()
-print(f"SUCCESS: Received file to process: {args.file_path}")
-print("--- Cloud Build Python script has finished ---")
